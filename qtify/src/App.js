577e836420090b95720281b1f7ccca8d5087e897
Debmalya Banerjee <98258452+SSRThePhoton@users.noreply.github.com>
2025-03-15 14:01:13 +0530

    commit done

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
-// import SongsSection from './components/SongsSection/SongsSection';
-// import NewAlbumsSection from './components/NewAlbumsSection/NewAlbumsSection';
-// import FAQ from './components/Faq/FAQ';
+import SongsSection from './components/SongsSection/SongsSection';
+import NewAlbumsSection from './components/NewAlbumsSection/NewAlbumsSection';
+import FAQ from './components/Faq/FAQ';
 
 function App() {
   return (
     <div className="App" style={{ backgroundColor: 'black', minHeight: '100vh', color: 'white' }}>
       <Navbar />
       <HeroSection />
-      {/* <NewAlbumsSection />
+      <NewAlbumsSection />
       <SongsSection />
-      <FAQ/> */}
+      <FAQ/>
     </div>
   );
 }
